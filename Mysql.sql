a0c46eea0b87d4ffcdf41bbf26a2e1d6d6558ede
Wesley Thauan <thauanblox@gmail.com>
2024-12-20 08:02:09 -0300

    Mysql2.sql

--- Mysql.sql
+++ Mysql.sql
@@ -1,2 +1,12 @@
-# Mysql2
-Mysql2
+CREATE TABLE Produtos (
+    ProdutoID INT AUTO_INCREMENT PRIMARY KEY,
+    NomeProduto VARCHAR(100) NOT NULL,
+    Quantidade INT NOT NULL,
+    Preco DECIMAL(10, 2) NOT NULL
+);
+
+INSERT INTO Produtos (NomeProduto, Quantidade, Preco)
+VALUES 
+    ('Teclado Mecânico', 50, 199.99),
+    ('Mouse Gamer', 30, 129.50),
+    ('Monitor 24 Polegadas', 20, 899.90);
